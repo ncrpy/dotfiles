1dafeabab2783d8f1f347d15dafd2d317c887da9
ncrpy <61719652+ncrpy@users.noreply.github.com>
2026-01-21 15:57:50 +0900

    Update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,9 +9,10 @@ else
 end
 
 config.font = wezterm.font_with_fallback({
-  "PlemolJP Console NF",
+  { family = "PlemolJP Console NF", weight = "Medium" },
 })
 config.font_size = 13.0
+config.line_height = 1.2
 
 config.use_ime = true
 
@@ -19,26 +20,44 @@ config.color_scheme = "Catppuccin Mocha"
 
 local scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
 
-config.colors = {
-  tab_bar = {
-    active_tab = {
-      bg_color = scheme.background,
-      fg_color = scheme.foreground,
-    },
-    new_tab_hover = {
-      bg_color = "none",
-      fg_color = scheme.foreground,
-    },
-  },
-}
+if scheme.tab_bar ~= nil then
+  config.window_frame = {
+    inactive_titlebar_bg = scheme.scrollbar_thumb,
+    active_titlebar_bg = scheme.tab_bar.new_tab.bg_color,
+    font_size = 11.0,
+  }
 
-config.window_frame = {
-  font_size = 11.0,
-}
+  scheme.tab_bar.new_tab.bg_color = "none"
+  config.colors = {
+    tab_bar = scheme.tab_bar,
+  }
+else
+  config.window_frame = {
+    font_size = 11.0,
+  }
+
+  config.colors = {
+    tab_bar = {
+      new_tab = {
+        bg_color = "none",
+        fg_color = "#808080",
+      },
+      new_tab_hover = {
+        bg_color = "1f1f1f",
+        fg_color = "#c0c0c0",
+      },
+    },
+  }
+end
 
 config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.adjust_window_size_when_changing_font_size = false
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.9
+
+config.inactive_pane_hsb = {
+  saturation = 0.8,
+  brightness = 0.7,
+}
 
 config.enable_scroll_bar = true
 
